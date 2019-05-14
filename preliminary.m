a90b620ce2e3f0a18ff28ac0da51c083ed59a211
unknown <dylancallaway@berkeley.edu>
2019-05-14 13:53:00 -0700

    simulink stuff

--- preliminary.m
+++ preliminary.m
@@ -5,14 +5,31 @@ clc
 %% Define motor parameters
 J = 0.01;
 b = 0.1;
-K = 0.1397;
-R = 1;
+K = 0.1397; % MMGM
+R = 7; % MMGM
 L = 0.5;
 s = tf('s');
 motor_tf = K / ( s*( L*J*s^2 + (L*b+R*J)*s + (R*b+K^2) ) )
 
 %% Simulate open loop system with step response
 % linearSystemAnalyzer('step', P_motor, 0:0.1:5)
+
+%% Check ctrb and obsv
+close all
+clc
+
+A = [0 1 0
+    0 -b/J K/J
+    0 -K/L -R/L];
+
+B = [0
+    0
+    1/L];
+
+C = [1 0 0];
+
+rank(ctrb(A, B))
+rank(obsv(A, C))
 
 %% Add control
 close all
@@ -32,5 +49,5 @@ step(cl_sys, t, opt)
 grid
 title('Step Response with Control')
 
-%% 
+%%
 close all
